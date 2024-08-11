8fd971d0f93cb5bdaabbc4bd3bb4a93730627991
lutencjusz <lutencjusz@gmail.com>
2024-08-11 15:55:50 +0200

    dodanie testów dla studentApiController

--- src/test/java/com/example/demo/ReactiveSpringApplicationTests.java
+++ src/test/java/com/example/demo/ReactiveSpringApplicationTests.java
@@ -1,13 +1,53 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
-@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ReactiveSpringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    Logger logger = LoggerFactory.getLogger(StudentApiController.class);
 
+    private final StudentApiController studentApiController = new StudentApiController();
+
+    /**
+     * Test sprawdza czy dane zwracane przez metodę get() z klasy StudentApiController są poprawne.
+     * Wymaga uruchomionego springa ReactiveSpringApplication
+     */
+    @Test
+    @Order(1)
+    void testControllerGet() {
+        Flux<Student> studentFlux = studentApiController.get();
+        studentFlux
+                .doOnNext(student -> logger.info(student.toString()))
+                .subscribe();
+        StepVerifier.create(studentFlux)
+                .expectNext(new Student(1, "John Doe", "25"))
+                .expectNext(new Student(2, "Jane Doe", "22"))
+                .expectNext(new Student(3, "John Smith", "30"))
+                .verifyComplete();
+    }
+
+    /**
+     * Test sprawdza czy dane zwracane przez metodę createStudent() z klasy StudentApiController są poprawne.
+     * Wymaga uruchomionego springa ReactiveSpringApplication
+     */
+    @Test()
+    @Order(2)
+    void testControllerCreateStudent() {
+        Mono<Student> studentMono = studentApiController.createStudent(new Student(4, "Paweł", "30"));
+        studentMono
+                .doOnNext(student -> logger.info(student.toString()))
+                .subscribe();
+        StepVerifier.create(studentMono)
+                .expectNext(new Student(4, "Paweł", "30"))
+                .verifyComplete();
+    }
 }
